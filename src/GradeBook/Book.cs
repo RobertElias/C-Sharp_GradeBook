661318fd5ee4925c8e57206b699a7b9b7faf72dc
Robert Elias <RobertElias@users.noreply.github.com>
2019-06-15 14:10:28 -0700

    Update class

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,7 @@
+namespace Gradebook 
+{
+    class Book 
+    {
+        
+    }
+}
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,13 +6,13 @@ namespace GradeBook
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            var book = new Book();
+            book.AddGrade(89.1);
             
-            var numbers = new[] {12.7, 10.3, 6.11, 4.1};
             var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
             grades.Add(56.1);
-
-                
+            
 
             var result = 0.0;
             foreach(var number in grades)
@@ -20,9 +20,7 @@ namespace GradeBook
                 result += number / grades.Count;
             }
             result /= grades.Count;
-            Console.WriteLine($"The average grade is, {result:N1}");
-
-            
+            Console.WriteLine($"The average grade is, {result:N1}");   
         }
     }
 }
