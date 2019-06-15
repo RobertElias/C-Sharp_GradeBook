dec3495fd6b102f817720fdd61e7ea2a37952a73
Robert Elias <RobertElias@users.noreply.github.com>
2019-06-15 13:49:50 -0700

    Update

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -11,23 +11,18 @@ namespace GradeBook
             var numbers = new[] {12.7, 10.3, 6.11, 4.1};
             var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
             grades.Add(56.1);
-            
+
+                
 
             var result = 0.0;
             foreach(var number in grades)
             {
                 result += number / grades.Count;
             }
+            result /= grades.Count;
             Console.WriteLine($"The average grade is, {result:N1}");
 
-            if(args.Length > 0)
-            {
-            Console.WriteLine($"Hello, {args[0]}!");
-            }
-            else
-            {
-            Console.WriteLine("Hello Robert Elias");    
-            }
+            
         }
     }
 }
