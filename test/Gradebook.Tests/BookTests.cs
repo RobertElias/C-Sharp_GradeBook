db7f2020b4f600441e6b1ec5aec6d3125c9a8dd9
Robert Elias <RobertElias@users.noreply.github.com>
2019-06-17 20:15:35 -0700

    Updates

---
+++ test/Gradebook.Tests/BookTests.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+
+namespace Gradebook.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //arrange
+            var book = new Book("");
+            
+
+            //act
+             
+
+            //assert
+            
+
+        }
+    }
+}
